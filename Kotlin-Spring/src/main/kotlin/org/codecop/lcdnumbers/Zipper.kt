f5235dd5ec8889d0825956f91b022b3a6a2f1709
Nat Pryce <sw@natpryce.com>
2020-06-25 17:07:07 +0100

    Remove unused interface

--- Kotlin-Spring/src/main/kotlin/org/codecop/lcdnumbers/Zipper.kt
+++ Kotlin-Spring/src/main/kotlin/org/codecop/lcdnumbers/Zipper.kt
@@ -4,10 +4,5 @@ package org.codecop.lcdnumbers
  * Zip joins elements of collections element wise, i.e. all first elements are joined and so on.
  */
 interface Zipper {
-    @FunctionalInterface
-    interface Combiner<T, R> {
-        fun combine(t: T): R
-    }
-    
     fun <T, R> zip(collections: Collection<Collection<T>>, combine: (List<T>)-> R): List<R>
 }
