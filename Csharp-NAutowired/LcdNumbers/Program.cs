6ded4f816be9679f7630fb1cade987184354e959
Peter Kofler <peter.kofler@code-cop.org>
2020-06-29 00:23:06 +0200

    Add command line help.

--- Csharp-NAutowired/LcdNumbers/Program.cs
+++ Csharp-NAutowired/LcdNumbers/Program.cs
@@ -5,11 +5,17 @@ using System.Threading.Tasks;
 
 namespace Org.Codecop.Lcdnumbers
 {
-    /// <summary>Fake client code. Run this class to see LCD Numbers working.</summary>
     public class Program
     {
         public static void Main(string[] args)
         {
+            if (args.Length == 0) {
+                Console.WriteLine("Run this class to see LCD Numbers working:");
+                Console.WriteLine("\nRunning via dotnet:");
+                Console.WriteLine("dotnet run --project LcdNumbers 12345 2");
+                return;
+            }
+
             LcdDisplay lcdDisplay = new LcdDisplay( //
                 new DigitsSplitter(new NumeralSystem(), new DigitFactory(new Patterns())), //
                 new DigitScaler(new ScalingRepeater()), //
